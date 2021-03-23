87e7e0065b3a62724c945b71b88890f6ce1e4f2b
Julia Sidor <juliasidor811@gmail.com>
2021-03-23 17:25:29 +0100

    7.5 poprawa

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,16 +8,10 @@ public class ArrayOperationsTestSuite {
     @Test
     void testGetAverage() {
 
-        int[] givenArray = new int[10];
-        int sum = 0;
-        for (int i = 0; i < givenArray.length; i++) {
-            int value = (int)(Math.random()*101);
-            givenArray[i] = value;
-            sum += value;
-        }
-        double expectedAvg = (double) sum/givenArray.length;
+        int[] givenArray = {1,4,3,2};
+        double expectedAvg = 2.5;
         double resultAvg = ArrayOperations.getAverage(givenArray);
-       Assertions.assertEquals(expectedAvg,resultAvg);
+         Assertions.assertEquals(expectedAvg,resultAvg);
 
 
     }
